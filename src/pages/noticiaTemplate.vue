7d3b3a3e54e631e0717ee8c3f7394141aa45f885
GuinneverC <guinnevercorrea.1@gmail.com>
2022-01-24 16:04:03 -0300

    changing-props

--- src/pages/noticiaTemplate.vue
+++ src/pages/noticiaTemplate.vue
@@ -3,13 +3,22 @@
         <Header/>
         <v-main>
           <v-container class="titulo-noticia">
-          <div class="d-flex justify-center flex-column" id="teste">
+          <div class="d-flex justify-center flex-column" id="teste" v-if="noticia">
             <h1>{{noticia.titulo}}</h1>
-            <span>{{noticia.data.toDate()}}</span>
+            <span>{{noticia.data}}</span>
             <hr class="red">
             <v-container class="conteudo">
             <span v-html="noticia.corpo"></span>
             <v-img :src="noticia.imgChamada"></v-img>
+            </v-container>
+          </div>
+          <div class="d-flex justify-center flex-column" id="teste" v-else>
+            <h1>{{noticia1.titulo}}</h1>
+            <span>{{noticia1.data.toDate()}}</span>
+            <hr class="red">
+            <v-container class="conteudo">
+            <span v-html="noticia1.corpo"></span>
+            <v-img :src="noticia1.imgChamada"></v-img>
             </v-container>
           </div>
           </v-container>
@@ -25,28 +34,20 @@ export default {
     components: {
         Header,
     },
+    data(){
+      return{
+        noticia1: {}
+      }
+    },
     created() {
       console.log(this.$route.params.id)
       if (this.noticia === undefined) {
         this.getNews()
-        
-        
-
-//for (const doc of logNews.docs) {
-        //  this.noticias.push({
-        //  id: doc.id,
-        //  data: doc.data().data.toDate(),
-        //  titulo: doc.data().titulo,
-        //  corpo: doc.data().corpo,
-        //  imgChamada: doc.data().imgChamada,
-        //  topico: doc.data().topico
-        //    });
-        //}
       }
     }, methods: {
       async getNews() {
         await firebase.database.collection("news").doc(this.$route.params.id).get().then( (doc)=>{
-          this.noticia = doc.data()
+          this.noticia1 = doc.data()
           console.log(doc.data())
           });
       }
